d197b5bf83fa02afbc7c8ec459c41f1f504520ad
Yusuf Önder <94531855+yusufnx1@users.noreply.github.com>
2023-10-22 21:07:49 +0300

    update - frontend finish

--- Frontend/HotelProject.WebUI/Controllers/DefaltController.cs
+++ Frontend/HotelProject.WebUI/Controllers/DefaltController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class DefaltController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaltController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult _SubscribePartial()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> _SubscribePartial(CreateSubscribeDto createSubscribe)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsondata = JsonConvert.SerializeObject(createSubscribe);
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            var responsiveMessage = await client.PostAsync("http://localhost:5069/api/Subscribe", stringContent);
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Defalt");
+            }
             return View();
         }
     }
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsiveMessage = await client.GetAsync("http://localhost:5069/api/Service");
+            if (responsiveMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsiveMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
