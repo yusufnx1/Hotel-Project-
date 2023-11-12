c538d7765de522fece1088e614a170916a285f81
Yusuf Önder <94531855+yusufnx1@users.noreply.github.com>
2023-11-12 19:31:05 +0300

    JWT Token

---
+++ JWT/JwtProjects/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using JwtProjects.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtProjects.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult TokunOlustur()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenOlustur()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Hoşgeldiniz");
+        }
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Başarılı Bir Şekilde Giriş Yaptı");
+        }
+    }
+}
